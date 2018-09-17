40857aca89c5fcb0e971b00d1cc720e6edbf76de
wc-szh <494727043@qq.com>
2018-09-17 17:47:28 +0800

    springboot+maven

---
+++ src/main/java/com/clarks/ClarksBootApplication.java
@@ -0,0 +1,12 @@
+package com.clarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarksBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClarksBootApplication.class, args);
+    }
+}
